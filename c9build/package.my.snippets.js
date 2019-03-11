5758316b36753a2d671fcfba9181a7764cdc5ec4
Richard O'flynn <richard.oflynn@brighter-software.co.uk>
2019-03-11 14:27:15 +0000

    Update

--- c9build/package.my.snippets.js
+++ c9build/package.my.snippets.js
@@ -25,7 +25,7 @@ define("plugins/my.snippets/__static__",[], function(require, exports, module) {
                 {
                     "type": "snippets",
                     "filename": "javascript.snippets",
-                    "data": "# scope: javascript\n\n\n# class method snippet\nsnippet meth\n\t\t/*-------------------------------------------------------------------------\n\t\t* ${1}\n\t\t*------------------------------------------------------------------------*/\n\t\t${2}(${3}) {\n\n\t\t\t${0}\n\t\t}\n\n# test snippet\nsnippet test\n\t\tif (${1:true}) {\n\t\t\t${2}\n\t\t}\n\t\t${0}\n\n# import snippet\nsnippet im\n\t\timport ${1} from '${2}'${0}\n\n# export snippet\nsnippet ex\n\t\texport default ${0}\n\n# class snippet\nsnippet class\n\t\t${1} class {\n\t\t\tconstructor(${2}) {\n\t\t\t\t${0}\n\t\t\t}\n\t\t}\n"
+                    "data": "# scope: javascript\n\n\n# class method snippet\nsnippet meth\n\t\t/*-------------------------------------------------------------------------\n\t\t* ${1}\n\t\t*------------------------------------------------------------------------*/\n\t\t${2}(${3}) {\n\n\t\t\t${0}\n\t\t}\n"
                 }
             ].forEach(function(x) {
                 debug.addStaticPlugin(x.type, "my.snippets", x.filename, x.data, plugin);
